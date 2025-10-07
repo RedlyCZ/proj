e499dbb6908dbb2a2998b696c6959cf7f1213589
Marek Pucejdl <marekpucejdl@gmail.com>
2025-10-07 12:10:37 +0200

    done at lab

--- labs/c02-pocitani_ovecek/pocitani_ovecek/pocitani_ovecek/pocitani_ovecek.cpp
+++ labs/c02-pocitani_ovecek/pocitani_ovecek/pocitani_ovecek/pocitani_ovecek.cpp
@@ -1,10 +1,51 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 #include <string>
+#include <cctype>
+#include <print>
 
 using namespace std;
 
+class SheepCounter {
+public:
+    int charCount;
+    int rowCount;
+    int wordCount;
+    int sentenceCount;
+    int numberCount;
+    int numberSum;
+    void processStream(istream& stream);
+private:
+    void processChar(char nextChar);
+    bool wordActive;
+    bool sentenceActive;
+    bool numberActive;
+};
+
+void SheepCounter::processStream(istream& stream) {
+    char c;
+    for (;;) {
+        c = stream.get();
+        if (stream.fail()) {
+            return;
+        }
+        processChar(c);
+    }
+}
+
+void SheepCounter::processChar(char nextChar) {
+    charCount++;
+    if (nextChar == '\n') {
+        rowCount++;
+    }
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    SheepCounter counter;
+    counter.charCount = 0;
+    counter.rowCount = 0;
+    counter.processStream(cin);
+    println("{}", counter.charCount);
+    println("{}", counter.rowCount);
 }
+
