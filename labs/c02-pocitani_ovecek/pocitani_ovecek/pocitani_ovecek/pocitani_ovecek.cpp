f31c73182ec7ac34b8913569ebdec3aaa5b9a71a
Marek Pucejdl <marekpucejdl@gmail.com>
2025-10-12 11:53:05 +0200

    finish house assignment after lab 02

--- labs/c02-pocitani_ovecek/pocitani_ovecek/pocitani_ovecek/pocitani_ovecek.cpp
+++ labs/c02-pocitani_ovecek/pocitani_ovecek/pocitani_ovecek/pocitani_ovecek.cpp
@@ -5,20 +5,26 @@
 
 using namespace std;
 
+struct counters {
+    int charCount = 0;
+    int rowCount = 0;
+    int wordCount = 0;
+    int sentenceCount = 0;
+    int numberCount = 0;
+};
+
 class SheepCounter {
 public:
-    int charCount;
-    int rowCount;
-    int wordCount;
-    int sentenceCount;
-    int numberCount;
-    int numberSum;
+    counters cntrs;
+    int numberSum = 0;
     void processStream(istream& stream);
+    void printInfo();
 private:
     void processChar(char nextChar);
-    bool wordActive;
-    bool sentenceActive;
-    bool numberActive;
+    bool wordActive = false;
+    bool sentenceActive = false;
+    bool numberActive = false;
+    int activeNmbValue = 0;
 };
 
 void SheepCounter::processStream(istream& stream) {
@@ -33,19 +39,63 @@ void SheepCounter::processStream(istream& stream) {
 }
 
 void SheepCounter::processChar(char nextChar) {
-    charCount++;
-    if (nextChar == '\n') {
-        rowCount++;
+    cntrs.charCount++; //Chars - MEL BY SE POCITAT I KONEC RADKY MEZI ZNAKY?
+    //Numbers
+    if (isdigit(nextChar)) {
+        numberActive = true;
+        activeNmbValue = (activeNmbValue * 10) + nextChar - '0'; //Horners method
     }
+    else {
+        if (numberActive) {
+            numberActive = false;
+            cntrs.numberCount++;
+            numberSum = numberSum + activeNmbValue;
+            activeNmbValue = 0;
+        }
+    }
+    //Words
+    if (isalpha(nextChar)) {
+        wordActive = true;
+    }
+    else {
+        if (wordActive) {
+            wordActive = false;
+            cntrs.wordCount++;
+        }
+    }
+    //Sentences
+    if (isupper(nextChar)) {
+        sentenceActive = true;
+    }
+    else {
+        if (nextChar == '.' && sentenceActive) {
+            cntrs.sentenceCount++;
+            sentenceActive = false;
+        }
+    }
+    //Rows
+    if (nextChar == '\n') {
+        cntrs.rowCount++;
+        if (wordActive) {
+            cntrs.wordCount++;
+        }
+    }
+}
+
+void SheepCounter::printInfo() {
+    println("In the stream, there were:");
+    println("   {} characters", cntrs.charCount);
+    println("   {} rows", cntrs.rowCount);
+    println("   {} words", cntrs.wordCount);
+    println("   {} numbers", cntrs.numberCount);
+    println("   {} sentences", cntrs.sentenceCount);
+    println("The sum of all the numbers was {}", numberSum);
 }
 
 int main()
 {
     SheepCounter counter;
-    counter.charCount = 0;
-    counter.rowCount = 0;
     counter.processStream(cin);
-    println("{}", counter.charCount);
-    println("{}", counter.rowCount);
+    counter.printInfo();
 }
 
