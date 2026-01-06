379cb163630355e8e71afb3e4cb6a2c889915894
Marek Pucejdl <marekpucejdl@gmail.com>
2026-01-06 15:30:23 +0100

    finish lab work implementing double adder with fractions

--- labs/c09-polymorfni_struktury/zlomky_sablony/zlomky_sablony/zlomky_sablony.cpp
+++ labs/c09-polymorfni_struktury/zlomky_sablony/zlomky_sablony/zlomky_sablony.cpp
@@ -1,4 +1,8 @@
 ﻿#include <print>
+#include <string>
+#include <iostream>
+
+using namespace std;
 
 template<typename T> class zlomek
 {
@@ -9,10 +13,11 @@ public:
     }
     template<typename X> friend zlomek<X> operator+(const zlomek<X>& a, const zlomek<X>& b);
 
-    void vypisse() {
-        println("{}", this->citatel);
-        println("{}", this->jmenovatel);
+    friend std::ostream& operator<<(std::ostream& os, const zlomek<T>& z) {
+        os << z.citatel << "/" << z.jmenovatel;
+        return os;
     }
+
 private:
     T citatel;
     T jmenovatel;
@@ -23,12 +28,49 @@ zlomek<X> operator+(const zlomek<X>& a, const zlomek<X>& b) {
     return zlomek<X> {a.citatel* b.jmenovatel + b.citatel * a.jmenovatel, a.jmenovatel* b.jmenovatel};
 }
 
+template<typename T> class DA {
+public:
+    DA(T v) : v_(v) {}
+
+    template<typename X>
+    friend DA<X> operator+(const DA<X>& x, const DA<X>& y);
+
+    void print() {
+        std::cout << v_ << std::endl;
+    }
+
+private:
+    T v_;
+};
+
+template<typename X>
+DA<X> operator+(const DA<X>& x, const DA<X>& y) {
+    // The logic from the slide
+    return DA<X> { x.v_ + x.v_ + y.v_ + y.v_ };
+}
+
 int main()
 {
-    zlomek<int> prvni = zlomek<int>(1, 2);
-    zlomek<int> druhy = zlomek<int>(2, 3);
-    zlomek<int> treti = prvni + druhy;
-    treti.vypisse();
+    DA<int> a = 2;
+    DA<int> b = 3;
+    DA<int> c = a + b;
+    c.print();
+
+    DA<double> d = 0.5687;
+    DA<double> e = 0.4313;
+    DA<double> f = d + e;
+    f.print();
+
+    DA<string> g = string("Hi");
+    DA<string> h = string("Ye");
+    DA<string> ch = g + h;
+    ch.print();
+
+    DA<zlomek<int>> i = zlomek<int>(1, 2);
+    DA<zlomek<int>> j = zlomek<int>(2, 3);
+    DA<zlomek<int>> k = i + j;
+    k.print();
+
 
 }
 
