b1329233e8d7a521c6392b65f030819dd725e4e9
Marek Pucejdl <marekpucejdl@gmail.com>
2026-04-01 23:22:35 +0200

    add signatures of CLIManager methods, add new methods to CLIPrinter

--- project/src/CLIPrinter.hpp
+++ project/src/CLIPrinter.hpp
@@ -14,6 +14,12 @@ public:
 	void printCryptoPositions(const RTPortfolio& pf, bool perf);
 	void printPortfolioPositions(const RTPortfolio& pf, bool performances);
 	void printCumulativeMetrics(double totalValue, double stocksVal, double cashesVal, double cryptoVal, double totalPerf);
+	void printPerformanceReport(const perfRatios& perf);
+	void printBacktestResult(const perfRatios& perf, double totalPerf, const std::chrono::year_month_day& date);
+	void printYieldPrediction(const perfRatios& perf, double years);
+	void printIndicatorRSI(const std::string& ticker, double rsi);
+	void printIndicatorBollinger(const std::string& ticker, int bollingerState);
+	void printMonteCarloResult(const std::string& ticker, int duration, double targetPrice, double chance);
 };
 
 #endif
---
+++ project/src/userInterface.hpp
@@ -0,0 +1,36 @@
+#ifndef USER_INTERFACE
+#define USER_INTERFACE
+
+#include "runtimePortfolio.hpp"
+#include "financialCalc.hpp"
+#include "CLIPrinter.hpp"
+#include <string>
+#include <chrono>
+
+class CLIManager {
+private:
+	RTPortfolio portfolio;
+	bool failed = false;
+	void showAggregates() const;
+	void showPositions() const;
+	void showPerformance() const;
+	void backtestPortfolio(const std::chrono::year_month_day& timeSince) const;
+	void buy(instrumentType type, const std::string& newTicker, double newQuantity);
+	void sell(instrumentType type, const std::string& newTicker, double newQuantity, bool closePosition);
+	void depositUSD(double quantity);
+	void cashoutUSD(double quantity);
+	void fixYieldPrediction(double years) const;
+	void calcRSI(instrumentType type, const std::string& ticker, int period) const;
+	void calcBollinger(instrumentType type, const std::string& ticker) const;
+	void monteCarloChance(instrumentType type, const std::string& ticker, int duration, double price, bool hit) const;
+	void loadPF(const std::chrono::year_month_day& date);
+	void savePF() const;
+public:
+	void interpretInputRow(const std::string& inputRow);
+	bool is_failed() const noexcept {
+		return failed;
+	}
+};
+
+
+#endif
